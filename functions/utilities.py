fd65b28cdd0d89f3f7ae22833b569e3aba11dda1
Liyuan Hu <41933694+zaza0209@users.noreply.github.com>
2024-05-31 18:04:13 +0100

    update IC

--- functions/utilities.py
+++ functions/utilities.py
@@ -15,51 +15,7 @@ def h_in_IC(changepoints, T, h='1'):
         return (np.sqrt(np.sum(T - 1 - changepoints) / np.log(np.sum(T - 1 - changepoints))))
 
 
-<<<<<<< Updated upstream
-# def IC(loss, changepoints, g_index, N, T, K, C=2, loss_fun='no_scale', Kl_fun='Nlog(NT)/T', h='1', C_K = 0):
-#     '''
-#     Parameters
-#     ----------
-#     Kl_fun : Penalty on K. The default is 'NlogNT/T'. In older version of IC, this is 'logN'
-#     h : TYPE, optional
-#         DESCRIPTION. The default is '1'.
-#     C_K: The coefficient in h function. Since we no long have the h term in the IC, the default is 0.
-#     C : The coefficient in the penalty function Kl_fun. The default is 2. Note that I'm not sure how to set the default value.
-#           In theory, this value should be of order O(log(NT)). I've tried on my side C_K=2 works well in an example where the penalty function with C_K=2 happends to equal to that in the BIC.
-#           If C=2 doesn't work for you, you may consider change it to something else like O(log(NT)), O(sqrt(log(NT))), O(NT).
-#
-#     '''
-#     # K = len(set(g_index))
-#     print("Kl_fun = ", Kl_fun)
-#     print('K', K, 'N', N)
-#     if Kl_fun == 'log':
-#         Kl = K * np.log(np.sum(T - 1 - changepoints))
-#     elif Kl_fun == "sqrt":
-#         Kl = K * np.sqrt(np.sum(T - 1 - changepoints))
-#     elif Kl_fun == 'logN':
-#         Kl = K * np.log(N)
-#     elif Kl_fun == 'Nlog(NT)/T':
-#         Kl = C* K* N * np.log(N*T)/T
-#     # print("kl", Kl)
-#     _, indicesList, occurCount = np.unique(g_index, return_index=True, return_counts=True)
-#     # print('c',occurCount.dot((T-1 -changepoints)[np.s_[indicesList]])/(N*T) * C * np.log(np.sum(T-1 -changepoints)))
-#     if loss_fun == 'no_scale':
-#         loss = loss / np.mean(T - 1 - changepoints)
-#     print('loss', loss)
-#     # print('(T-1 -changepoints)[np.s_[indicesList]]',(T-1 -changepoints)[np.s_[indicesList]])
-#     # print('np.log(np.sum(T-1 -changepoints)',np.log(np.sum(T-1 -changepoints)))
-#     print("ic",
-#           loss - Kl + occurCount.dot((T - 1 - changepoints)[np.s_[indicesList]]) / (N * T) * C_K * h_in_IC(changepoints,
-#                                                                                                          T), ', ', K,
-#           '*l', Kl, ', ', C, '* h=',
-#           C_K * occurCount.dot((T - 1 - changepoints)[np.s_[indicesList]]) / (N * T) * h_in_IC(changepoints, T))
-#     return loss - Kl + occurCount.dot((T - 1 - changepoints)[np.s_[indicesList]]) / (N * T) * C_K * h_in_IC(changepoints,
-
-
-def IC(loss, changepoints, g_index, N, T, K, C=0, loss_fun='no_scale', Kl_fun='Nlog(NT)/T', h='1', C_K = 0):
-=======
 def IC(loss, changepoints, g_index, N, T, K, C=1, loss_fun='no_scale', Kl_fun="sqrtN", h='1', C_K = 0):
->>>>>>> Stashed changes
     '''
     Parameters
     ----------
@@ -68,17 +24,14 @@ def IC(loss, changepoints, g_index, N, T, K, C=1, loss_fun='no_scale', Kl_fun="s
     h : TYPE, optional
         DESCRIPTION. The default is '1'.
     C_K: The coefficient in h function. Since we no long have the h term in the IC, the default is 0.
-    C : The coefficient in the penalty function Kl_fun. The default is 2. Note that I'm not sure how to set the default value.
+    C : The coefficient in the penalty function Kl_fun. The default is 2. Note that I'm not sure how to set the default value. 
           In theory, this value should be of order O(log(NT)). I've tried on my side C_K=2 works well in an example where the penalty function with C_K=2 happends to equal to that in the BIC.
           If C=2 doesn't work for you, you may consider change it to something else like O(log(NT)), O(sqrt(log(NT))), O(NT).
-<<<<<<< Updated upstream
-=======
           
->>>>>>> Stashed changes
     '''
     # K = len(set(g_index))
     print("Kl_fun = ", Kl_fun)
-    print('K =', K, ', N =', N)
+    print('K', K, 'N', N)
     if Kl_fun == 'log':
         Kl = K * np.log(np.sum(T - 1 - changepoints))
     elif Kl_fun == "sqrt":
@@ -86,25 +39,24 @@ def IC(loss, changepoints, g_index, N, T, K, C=1, loss_fun='no_scale', Kl_fun="s
     elif Kl_fun == 'logN':
         Kl = K * np.log(N)
     elif Kl_fun == 'Nlog(NT)/T':
-<<<<<<< Updated upstream
-        Kl = C * K * N * np.log(N*T)/T
-=======
         Kl = C* K* N * np.log(N*T)/T
     elif Kl_fun =="sqrtN":
         Kl = C* K* np.sqrt(N)
->>>>>>> Stashed changes
     # print("kl", Kl)
     _, indicesList, occurCount = np.unique(g_index, return_index=True, return_counts=True)
     # print('c',occurCount.dot((T-1 -changepoints)[np.s_[indicesList]])/(N*T) * C * np.log(np.sum(T-1 -changepoints)))
     if loss_fun == 'no_scale':
         loss = loss / np.mean(T - 1 - changepoints)
-    print('loss =', loss)
+    print('loss', loss)
     # print('(T-1 -changepoints)[np.s_[indicesList]]',(T-1 -changepoints)[np.s_[indicesList]])
     # print('np.log(np.sum(T-1 -changepoints)',np.log(np.sum(T-1 -changepoints)))
-    Ch = occurCount.dot((T - 1 - changepoints)[np.s_[indicesList]]) / (N * T) * C_K * h_in_IC(changepoints, T, h=h)
-    ic = loss - Kl + Ch
-    print("ic =", ic, ', ', K, '* l =', Kl, ', ', C, '* h =', Ch)
-    return ic
+    print("ic",
+          loss - Kl + occurCount.dot((T - 1 - changepoints)[np.s_[indicesList]]) / (N * T) * C_K * h_in_IC(changepoints,
+                                                                                                         T), ', ', K,
+          '*l', Kl, ', ', C, '* h=',
+          C_K * occurCount.dot((T - 1 - changepoints)[np.s_[indicesList]]) / (N * T) * h_in_IC(changepoints, T))
+    return loss - Kl + occurCount.dot((T - 1 - changepoints)[np.s_[indicesList]]) / (N * T) * C_K * h_in_IC(changepoints,
+                                                                                                          T, h=h)
 
 
 def paramInIC(model, N, K, T, include_path_loss=0):
